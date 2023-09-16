05067fa341c472c420cee13e5586924adb6e02f4
Ikasan <neconeco_festival@yahoo.co.jp>
2023-09-16 10:49:19 +0900

    トップページ編集

--- src/TopPage.tsx
+++ src/TopPage.tsx
@@ -1,42 +1,39 @@
 import React from 'react';
 import styled from '@emotion/styled'
-import logo from './logo.svg';
-import './App.css';
-import { Components } from './Components/Components';
 
-
-const Button = styled.button`
-  padding: 32px;
-  background-color: hotpink;
-  font-size: 24px;
-  border-radius: 4px;
-  color: black;
-  font-weight: bold;
-  &:hover {
-    color: white;
-  }
+const StyledTopPage = styled.div`
+  padding-top: 60px;
 `
+
+const StyledTopH1 = styled.h1`
+`
+
+const StyledTopH2 = styled.h2`
+`
+
+const StyledTopH3 = styled.h3`
+`
+
+const StyledTopP = styled.p`
+`
+
 
 function TopPage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button/>
-        <Components name="竹内"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledTopPage>
+      <StyledTopH1>Welocome to SF6FS</StyledTopH1>
+      <StyledTopH2>About this website</StyledTopH2>
+      <StyledTopP>STREET FIGHTER6のフレームやヒットボックス等、確認が面倒だったりすぐに表示されないデータを簡略にまとめ閲覧することが可能な攻略系ウェブサイトです。</StyledTopP>
+      <StyledTopH3>Characters</StyledTopH3>
+      <StyledTopP>キャラクターを一覧から選択し、通常技や特殊技等のフレームデータを確認できます。</StyledTopP>
+      <StyledTopP>また、技名部分をクリックすることでその技のヒットボックスの参照GIFを閲覧することも可能です。</StyledTopP>
+      <StyledTopH3>FramesVS</StyledTopH3>
+      <StyledTopP>防御側キャラクターと攻撃側キャラクターを選択し、攻撃側の行動(立ち弱P等)を選択することで不利フレームから確定する技を一覧表示することができます。</StyledTopP>
+      <StyledTopH3>Tier</StyledTopH3>
+      <StyledTopP>最新の情報を参考にキャラクターのTierランキングを表示します。</StyledTopP>
+      <StyledTopH3>Tips</StyledTopH3>
+      <StyledTopP>SF6をプレイするにあたって便利な知識等を掲載していくページです。</StyledTopP>
+    </StyledTopPage>
   );
 }
 
